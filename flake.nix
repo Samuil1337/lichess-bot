e4938683af86921566da7f647215e4f2c334c997
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-02-25 17:26:56 +0100

    Nix: Add dev shells as Flake

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Flake for the Laser-AI development shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      system = system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+
+        packages = [
+          (pkgs.python312.withPackages(p: with p; [
+            # lichess-bot
+            pyyaml
+            backoff
+            rich
+	    # Laser-AI
+            numpy
+            chess
+            torch
+            pandas
+            requests
+            fastapi
+            fastapi-cli
+          ]))
+        ];
+
+      };
+      cuda = pkgs.mkShell {
+
+        packages = [
+          (pkgs.python312.withPackages(p: with p; [
+            # lichess-bot
+            pyyaml
+            backoff
+            rich
+	    # Laser-AI
+            numpy
+            chess
+            torch-bin  # precompiled PyTorch with CUDA support
+            pandas
+            requests
+            fastapi
+            fastapi-cli
+          ]))
+        ];
+
+      };
+    };
+  };
+}
